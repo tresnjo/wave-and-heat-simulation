70133d0d51aacc6331c4ff4de560bbaac69d6279
Thidius <121384892+Thidius@users.noreply.github.com>
2023-01-29 21:44:45 +0100

    Update Finite Difference Method.py

--- Finite Difference Method.py
+++ Finite Difference Method.py
@@ -4,11 +4,8 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import matplotlib as mpl
 
-
-
-def f(X,Y): # f(x,y,0)
+def f(X,Y): # Initial condition - f(x,y,0)
     return 3-X**2-Y**2+np.sin(X*Y)
-
 
 def simulation_heat_eq(rect,hs,BC, c, frames, eps = 1e-10):
 
@@ -18,12 +15,10 @@ def simulation_heat_eq(rect,hs,BC, c, frames, eps = 1e-10):
     X, Y = np.meshgrid(np.linspace(rect[0],rect[1],hs[0]),
                        np.linspace(rect[2],rect[3],hs[1])) # 2D meshgrid
 
-    # Initial Conditions
+    # Boundary & Initial Conditions
     Z_init = f(X,Y) # f(x,y,0)
     zmax = max(Z_init.max(), BC[0], BC[1], BC[2], BC[3])
     zmin = min(Z_init.min(), BC[0], BC[1], BC[2], BC[3])
-
-    # Boundary Conditions
     Z_init[0] = np.ones(len(Z_init[0])) * BC[0]
     Z_init[-1] = np.ones(len(Z_init[-1]))* BC[1]
     Z_init[:,0] =  np.ones(len(Z_init[:,0]))* BC[2]
@@ -38,7 +33,7 @@ def simulation_heat_eq(rect,hs,BC, c, frames, eps = 1e-10):
     plt.rcParams['mathtext.fontset'] = 'stix'
     plt.rcParams['font.family'] = 'STIXGeneral'
 
-    # Plots for lines constituting the rectangle
+    # Plots for lines constituting the rectangular room
     ax.plot([rect[0], rect[1]], [rect[2], rect[2]], [BC[0], BC[0]], color='black', linewidth=2)
     ax.plot([rect[1], rect[1]], [rect[2], rect[3]], [BC[3], BC[3]], color='black', linewidth=2)
     ax.plot([rect[0], rect[0]], [rect[2], rect[3]], [BC[2], BC[2]], color='black', linewidth=2)
@@ -54,7 +49,8 @@ def simulation_heat_eq(rect,hs,BC, c, frames, eps = 1e-10):
     # Infitesimals
     dx = (rect[1] - rect[0]) / (hs[0] - 1)
     dt = dx**2/(5*c**2)
-
+    
+    # Initial surface plot with colorbar
     surf = ax.plot_surface(X, Y, Z_init, alpha=0.7, cmap=plt.cm.jet, vmin=zmin, vmax=zmax)
     cbar = fig.colorbar(surf)
     cbar.ax.set_ylabel('Temperature in ' + r'$^\circ\mathrm{C}$', rotation=270,fontsize = 14, labelpad=20)
